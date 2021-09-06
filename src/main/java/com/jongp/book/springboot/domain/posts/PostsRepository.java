0a68c30ca87af287e26e9d8535460463c9e6e2bc
JongP <jong.p@kaist.ac.kr>
2021-09-06 20:54:02 +0900

    PostsApiControllerTest initial

---
+++ src/main/java/com/jongp/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jongp.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long>{
+
+}
---
+++ src/main/java/com/jongp/book/springboot/services/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jongp.book.springboot.services.posts;
+
+import com.jongp.book.springboot.domain.posts.PostsRepository;
+import com.jongp.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
